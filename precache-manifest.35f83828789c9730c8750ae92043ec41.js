96d61c67c76230e37f6bb72bbbcbf1d30b173a9a
VKPYpy <49330371+VKPYpy@users.noreply.github.com>
2020-02-06 18:04:43 +0300

    Add files via upload

---
+++ precache-manifest.35f83828789c9730c8750ae92043ec41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f24a05e6f1e309e1194e725d849218",
+    "url": "vkpypy.github.io/index.html"
+  },
+  {
+    "revision": "9e46c2f5c4e8e58f9d21",
+    "url": "vkpypy.github.io/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "5a5f8008a6a0d178438c",
+    "url": "vkpypy.github.io/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "9e46c2f5c4e8e58f9d21",
+    "url": "vkpypy.github.io/static/js/2.2f024ebe.chunk.js"
+  },
+  {
+    "revision": "5a5f8008a6a0d178438c",
+    "url": "vkpypy.github.io/static/js/main.7457e14d.chunk.js"
+  },
+  {
+    "revision": "c1946a166d9afd8e66b8",
+    "url": "vkpypy.github.io/static/js/runtime-main.d669bf49.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "vkpypy.github.io/static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "vkpypy.github.io/precache-manifest.3e217d3977000b6278b4f8926d5ce494.js"
+  "vkpypy.github.io/precache-manifest.35f83828789c9730c8750ae92043ec41.js"
 );
 
 self.addEventListener('message', (event) => {
